4835c6c55a5e95a6499098cc0c10ba98a48627bc
Logan Campbell <lcampbellmcpherson@zendesk.com>
2017-08-25 14:56:57 +1000

    removed compiled app.js and whitespaced project.clj

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "A minimal example Zendesk App using reagent"
   :url "https://github.com/lcampbell-zendesk/minimal-reagent-app"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
   :min-lein-version "2.7.1"
 
@@ -17,30 +17,29 @@
   :source-paths ["src"]
 
   :cljsbuild {:builds
-              [{:id "dev"
+              [{:id           "dev"
                 :source-paths ["src"]
-                :figwheel {:on-jsload "minimal-reagent-app.core/on-js-reload"
-                           :open-urls ["http://localhost:3449/index.html"]}
+                :figwheel     {:on-jsload "minimal-reagent-app.core/on-js-reload"}
 
-                :compiler {:main minimal-reagent-app.core
-                           :asset-path "js/compiled/out"
-                           :output-to "resources/public/js/compiled/minimal_reagent_app.js"
-                           :output-dir "resources/public/js/compiled/out"
+                :compiler {:main                 minimal-reagent-app.core
+                           :asset-path           "js/compiled/out"
+                           :output-to            "resources/public/js/compiled/minimal_reagent_app.js"
+                           :output-dir           "resources/public/js/compiled/out"
                            :source-map-timestamp true
-                           :preloads [devtools.preload]}}
-               {:id "min"
+                           :preloads             [devtools.preload]}}
+               {:id           "min"
                 :source-paths ["src"]
-                :compiler {:output-to "resources/dist/assets/app.js"
-                           :main minimal-reagent-app.core
-                           :optimizations :advanced
-                           :pretty-print false}}]}
+                :compiler     {:output-to     "resources/dist/assets/app.js"
+                               :main          minimal-reagent-app.core
+                               :optimizations :advanced
+                               :pretty-print  false}}]}
 
   :figwheel {:css-dirs ["resources/public/css"]}
 
-  :profiles {:dev {:dependencies [[binaryage/devtools "0.9.4"]
-                                  [figwheel-sidecar "0.5.13"]
-                                  [com.cemerick/piggieback "0.2.2"]]
-                   :source-paths ["src" "dev"]
-                   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+  :profiles {:dev {:dependencies  [[binaryage/devtools "0.9.4"]
+                                   [figwheel-sidecar "0.5.13"]
+                                   [com.cemerick/piggieback "0.2.2"]]
+                   :source-paths  ["src" "dev"]
+                   :repl-options  {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
                    :clean-targets ^{:protect false} ["resources/public/js/compiled"
                                                      :target-path]}})
